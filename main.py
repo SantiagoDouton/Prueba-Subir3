067e0e203c9103f8f574f8a5a25ec67755642d7d
SantiagoDouton <douton.santiago@gmail.com>
2023-06-13 12:15:04 -0300

    Es Python pero subo para practicar

---
+++ main.py
@@ -0,0 +1,17 @@
+
+#Variables
+texto = "Repaso de Python"
+nombre = "Santiago"
+altura = "174cm"
+year = 2023
+
+print(texto)
+
+#Concatenación
+#Es el unir varias variables en 1
+texto = "Repaso de Python"
+nombre = "Santiago"
+altura = "174cm"
+year = 2023
+
+print(f"{texto} - ")
